40954195a78f2dd62e690995c2c3f1190434aef5
Eddie Kero <ekero@kth.se>
2022-09-15 11:27:17 +0200

    Name change to folder

---
+++ cyberKlara/target/debug/Cyber-Klara.d
@@ -0,0 +1,1 @@
+C:\Users\Razer\indaplus\ekero-task-2\Cyber-Klara\target\debug\Cyber-Klara.exe: C:\Users\Razer\indaplus\ekero-task-2\Cyber-Klara\src\main.rs
---
+++ cyberKlara/target/debug/deps/Cyber_Klara.d
@@ -0,0 +1,5 @@
+C:\Users\Razer\indaplus\ekero-task-2\Cyber-Klara\target\debug\deps\Cyber_Klara.exe: src\main.rs
+
+C:\Users\Razer\indaplus\ekero-task-2\Cyber-Klara\target\debug\deps\Cyber_Klara.d: src\main.rs
+
+src\main.rs:
